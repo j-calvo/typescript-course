a53345d711d77c61ad9478b7ff6a4514a842e44b
Jonathan Calvo <extern.jonathan.calvo@vw.com>
2026-06-11 07:56:46 -0600

    Original Code

---
+++ chats.d.ts
@@ -0,0 +1,8 @@
+export declare module "./chats.js" {
+  export type Chat = {
+    time: string;
+    message: string;
+  }
+  export function log(chats: Chat[]) : void;
+  export const chats: Chat[];
+}
